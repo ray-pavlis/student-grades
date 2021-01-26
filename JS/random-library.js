9be6091b17e758c90ed99b72d6171751f5b3e314
ray-pavlis <63063032+ray-pavlis@users.noreply.github.com>
2021-01-26 11:11:56 -0700

    Add files via upload

---
+++ JS/random-library.js
@@ -0,0 +1,18 @@
+// Library of Random Functions
+
+// Return a random decimal between low(inclusive) and high(exclusive)
+function randomDec(low, high) {
+    return Math.random() * (high - low) + low;
+}
+
+// Return a random interger between low(inclusive) and high(exclusive)
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+function randomRGB() {
+    let r = randomInt(0, 256);
+    let g = randomInt(0, 256);
+    let b = randomInt(0, 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
